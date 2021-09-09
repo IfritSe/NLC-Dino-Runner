367b57e0d612fdb53c0c0f15687549416ae318ab
Ignacio Ruben Villarroel Rodriguez <ignaciovillarroel48@gmail.com>
2021-09-09 19:55:10 -0400

    Clase 3: Dino
    Adición de las funciones de correr, saltar y agacharse.

--- nlc_dino_runner/components/game.py
+++ nlc_dino_runner/components/game.py
@@ -1,6 +1,8 @@
 import pygame
 
+from nlc_dino_runner.components.dinosaur import Dinosaur
 from nlc_dino_runner.utils.constants import TITLE, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, BG, FPS
+
 
 class Game:
     def __init__(self):
@@ -13,6 +15,7 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 360
         self.game_speed = 20
+        self.player = Dinosaur()
 
     def run(self):
         self.playing = True
@@ -28,12 +31,14 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
